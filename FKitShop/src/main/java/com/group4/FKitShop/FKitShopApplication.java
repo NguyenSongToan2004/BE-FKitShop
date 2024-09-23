efb9bb77b04f9b42738e357a44a8dfcb9d307935
Nguyễn Song Toàn <toannsse183104@fpt.edu.vn>
2024-09-23 16:13:08 +0700

    First commit - Create project base

---
+++ FKitShop/src/main/java/com/group4/FKitShop/FKitShopApplication.java
@@ -0,0 +1,13 @@
+package com.group4.FKitShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FKitShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FKitShopApplication.class, args);
+	}
+
+}
