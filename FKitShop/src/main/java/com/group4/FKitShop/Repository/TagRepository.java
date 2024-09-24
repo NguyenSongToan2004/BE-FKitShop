54b8354846c7b7c6f7d6afaab486c3eb016873a8
vy120 <vynntse183453@fpt.edu.vn>
2024-09-25 01:00:16 +0700

    crud tag commit
    crud tag, relationship 1:n with category, not yet exception

---
+++ FKitShop/src/main/java/com/group4/FKitShop/Repository/TagRepository.java
@@ -0,0 +1,11 @@
+package com.group4.FKitShop.Repository;
+
+
+import com.group4.FKitShop.Entity.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Integer> {
+
+}
---
+++ FKitShop/src/main/java/com/group4/FKitShop/Request/CreateTagRequest.java
@@ -0,0 +1,12 @@
+package com.group4.FKitShop.Request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class CreateTagRequest {
+
+    private String tagName;
+    private String description;
+
+}
