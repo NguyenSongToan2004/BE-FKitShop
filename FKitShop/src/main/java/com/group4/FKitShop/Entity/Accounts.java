3b5d897c3c0fdec5ae94d843bc99c4bb993e1b85
Nguyễn Song Toàn <toannsse183104@fpt.edu.vn>
2024-09-27 23:52:02 +0700

    CRUD account - first commit

--- FKitShop/src/main/java/com/group4/FKitShop/Entity/Accounts.java
+++ FKitShop/src/main/java/com/group4/FKitShop/Entity/Accounts.java
@@ -1,4 +1,43 @@
 package com.group4.FKitShop.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
 public class Accounts {
+    @Id
+    @NotNull
+    String accountID;
+
+    @NotNull
+    String password;
+
+    String image;
+
+    @NotNull
+    String fullName;
+
+    @NotNull
+    String email;
+
+    @NotNull
+    int status; // 0 : inactive ; 1: active
+
+    @NotNull
+    String role; // 0: admin ; 1: manager; 2: staff; 3: customer
+
+    @NotNull
+    Date createDate;
 }
