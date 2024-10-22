0a87a533d8e7b8bbc1511939ef3029cfdf9a71ff
mngdinh <minhdc.uni@gmail.com>
2024-10-22 08:57:05 +0700

    accservice config

--- FKitShop/src/main/java/com/group4/FKitShop/Service/AccountsService.java
+++ FKitShop/src/main/java/com/group4/FKitShop/Service/AccountsService.java
@@ -11,6 +11,9 @@ import com.group4.FKitShop.Response.AccountsResponse;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -24,7 +27,7 @@ import java.util.Optional;
 @Service
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @RequiredArgsConstructor
-public class AccountsService {
+public class AccountsService implements UserDetailsService {
 
     AccountsRepository accountsRepository;
     AccountsMapper accountsMapper;
@@ -60,6 +63,15 @@ public class AccountsService {
     }
 
 
-
-
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //load account
+        Accounts account = accountsRepository.findByemail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with email: "+username));
+        return new org.springframework.security.core.userdetails.User(
+                account.getEmail(),
+                account.getPassword(),
+                new ArrayList<>()
+        );
+    }
 }
